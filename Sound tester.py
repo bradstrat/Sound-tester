58a98f4b38ec461435ee2554d7656bbb76ed689b
Bradley Hull <hull.bradley@ymail.com>
2020-01-28 19:55:41 -0500

    1.02

--- Sound tester.py
+++ Sound tester.py
@@ -1,17 +1,21 @@
+#Import needed libs
 import winsound
 import time
 
-def yumsoundyeet():
+#Function to get and play th windows sound
+def windowsSound():
     winsound.PlaySound("Sound2.wav", winsound.SND_ASYNC)
 
-
-def beautifulHell():
+#Function to play the windows sound every half a second on a loop
+def Repeatstuff():
     while True:
         time.sleep(0.5)
-        yumsoundyeet()
+        windowsSound()
 
 
-
+#Prints directions to quit
 print("Press ctrl + C to close program. Thank you.")
-beautifulHell()
+
+#calls function
+Repeatstuff()
 
